f5d073504b5006c7860e9c9a30baf91a5fb279ed
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2020-03-04 15:24:29 +0100

    Reformatted code

---
+++ cli.ino
@@ -0,0 +1,63 @@
+/*
+  Author: Niek van Leeuwen
+  Date: 27-02-2020
+*/
+
+// this function looks for a match for the given command
+bool cliSearchMatch() {
+  bool matchFound = false;
+  for (int i = 0; i < commandTypeSize; i++) {
+    if (strcmp(command[i].name, cliBuffer[0]) == 0) {
+      // call the matched function
+      void (*func)() = command[i].func;
+      func();
+      matchFound = true;
+    }
+  }
+  return matchFound;
+}
+
+// this function clears the cli buffers
+void clearBuffers() {
+  for (int i = 0; i < 4; i++) {
+    memset(cliBuffer[i], NULL, BUFSIZE);
+  }
+
+  cliBufferCounter = 0;
+  cliBufferCounterArguments = 0;
+}
+
+// main command line function
+void commandLine() {
+  // wait for data to be available
+  if (Serial.available() > 0) {
+    int incomingChar = Serial.read();
+
+    // split the buffer on the space char
+    if (incomingChar == ' ' && cliBufferCounter > 0) {
+      cliBufferCounterArguments++;
+      cliBufferCounter = 0;
+    } else if (incomingChar == 13 || incomingChar == 10) {
+      if (cliBufferCounter > 0) {
+        // find match for the command
+        bool matchFound = cliSearchMatch();
+
+        // show list in case of no match
+        if (matchFound == false) {
+          Serial.print("Command '");
+          Serial.print(cliBuffer[0]);
+          Serial.println("' is not a known command.");
+          list();
+        }
+
+        clearBuffers();
+
+        // give ready signal
+        Serial.println("\nArduinOS 0.1 ready.\n==================");
+      }
+    } else {
+      cliBuffer[cliBufferCounterArguments][cliBufferCounter] = incomingChar; // store the incoming char
+      cliBufferCounter++; // increment where to write next
+    }
+  }
+}
--- main.ino
+++ main.ino
@@ -2,15 +2,15 @@
   Author: Niek van Leeuwen
   Date: 27-02-2020
 */
+
 const int BUFSIZE = 32;
 
-/* CLI */
+/* CLI data  */
 void commandLine();
-// Array to store the input from the CLI
-static char cliBuffer[BUFSIZE];
-static int cliBufferIndex = 0;
-static char input[4][BUFSIZE];
-static int inputIndex = 0;
+// array to store the input from the CLI
+static char cliBuffer[4][BUFSIZE];
+static int cliBufferCounter = 0;
+static int cliBufferCounterArguments = 0;
 
 // list of all commands
 void store();
@@ -55,23 +55,23 @@ void loop() {
 void store(){
   Serial.println("\nStore");
   Serial.print("File: ");
-  Serial.println(input[1]);
+  Serial.println(cliBuffer[1]);
   Serial.print("Size: ");
-  Serial.println(input[2]);
+  Serial.println(cliBuffer[2]);
   Serial.print("Data: ");
-  Serial.println(input[3]);
+  Serial.println(cliBuffer[3]);
 }
 
 void retrieve(){
   Serial.println("\nRetrieve");
   Serial.print("File: ");
-  Serial.println(input[1]);
+  Serial.println(cliBuffer[1]);
 }
 
 void erase(){
   Serial.println("\nErase");
   Serial.print("File: ");
-  Serial.println(input[1]);
+  Serial.println(cliBuffer[1]);
 }
 
 void files(){
@@ -85,7 +85,7 @@ void freespace(){
 void run(){
   Serial.println("\nRun");
   Serial.print("File: ");
-  Serial.println(input[1]);
+  Serial.println(cliBuffer[1]);
 }
 
 void list(){
@@ -100,95 +100,17 @@ void list(){
 void suspend(){
   Serial.println("\nSuspend");
   Serial.print("ID: ");
-  Serial.println(input[1]);
+  Serial.println(cliBuffer[1]);
 }
 
 void resume(){
   Serial.println("\nResume");
   Serial.print("ID: ");
-  Serial.println(input[1]);
+  Serial.println(cliBuffer[1]);
 }
 
 void kill(){
   Serial.println("\nKill");
   Serial.print("ID: ");
-  Serial.println(input[1]);
-}
-
-bool cliSearchMatch(){
-  // split the input string on the space char
-  int j = 0;
-  for(int i = 0; i < BUFSIZE; i++){
-    if(cliBuffer[i] == ' '){
-        inputIndex++;
-        j = 0;
-    }else{
-        input[inputIndex][j] = cliBuffer[i];
-        j++;
-    }
-  }
-  
-  bool matchFound = false;
-  for(int i = 0; i < commandTypeSize; i++){
-    if(strcmp(command[i].name, input[0]) == 0){
-      void (*func)() = command[i].func;
-      func();
-      matchFound = true;
-    }
-  }
-  return matchFound;
-}
-
-void clearBuffers(){
-  // clear buffers
-  for(int i = 0; i < BUFSIZE; i++){
-    cliBuffer[i] = 0;
-  }
-
-  for(int i = 0; i < 2; i++){
-    for(int j = 0; j < BUFSIZE; j++){
-      input[i][j] = 0;
-    }
-  }
-  
-  cliBufferIndex = 0;
-  inputIndex = 0;
-}
-
-void commandLine(){
-    // wait for data to be available
-  if (Serial.available() > 0) {
-    // check for buffer overflow
-    if(cliBufferIndex < BUFSIZE - 1){
-        int incomingChar = Serial.read();
-        if(incomingChar == 10 || incomingChar == 13){
-          // check if buffer has values
-          if(cliBufferIndex > 0){
-            // find match for the command
-            bool matchFound = cliSearchMatch();
-
-            // show list in case of no match
-            if(matchFound == false){
-              Serial.print("Command '");
-              Serial.print(cliBuffer);
-              Serial.println("' is not a known command.");
-              list();
-            }
-
-            clearBuffers();
-            
-            // give ready signal
-            Serial.println("\nArduinOS 0.1 ready.\n==================");      
-          }
-        }else{
-           cliBuffer[cliBufferIndex] = incomingChar; // store the incoming char
-           cliBufferIndex++; // increment where to write next
-        }
-    }else{
-      Serial.print("The maxium input is ");
-      Serial.print(BUFSIZE);
-      Serial.println(" chars long.");
-      clearBuffers();
-    }
-  }
+  Serial.println(cliBuffer[1]);
 }
